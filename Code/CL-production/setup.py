c2e2beb7618912c0997c8ca5f8b0882bb5f63d85
wfarmilo <waf25@cam.ac.uk>
2026-08-12 14:26:25 +0100

    distribute lammps input from template

--- Code/CL-production/setup.py
+++ Code/CL-production/setup.py
@@ -1,3 +1,11 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("temp", help = "Simulation temperature", type = int)
+
+args=parser.parse_args()
+
 import os
 from pathlib import Path
 import numpy as np
@@ -7,9 +15,14 @@ from ase.io import read as aread, write as awrite
 runtypes = ['p0m1', 'p1m0', 'p1m1']
 run_nums = np.arange(5)
 
+step_size = 8   #4fs between frames
+temperature = args.temp
+outprefix = f'T{temperature}'
+
 cwd = Path("./").resolve()
 assert cwd.name == "CL-production", f"Wrong directory: {str(cwd)}"
 data_dir = cwd.parents[1] /"Data"
+templates_dir = cwd / "templates"
 
 #Set up output directories for classical production run
 
@@ -46,3 +59,20 @@ for rt in runtypes:
                atom_style = "atomic", 
                specorder = ["H", "O"], 
                masses = True)
+
+        #Get lammps input
+        lammps_inp = templates_dir / "input.lmp"
+
+        with open(lammps_inp, "r") as li:
+            inptext = li.read()
+
+        #Swap out placeholders
+        inptext = inptext.replace(f"XXXTEMPXXX", f"{temperature}")
+        inptext = inptext.replace(f"XXXSTEPSIZEXXX", f"{step_size}")
+        inptext = inptext.replace(f"XXXMODELPATHXXX", str(templates_dir / "ch2o-dens-inv_swa-1-8.json"))
+        inptext = inptext.replace(f"XXXOUTPREFXXX", outprefix)
+
+        #Save lammps input
+        lammps_inp_out = workdir / f"input-{outprefix}.lmp"
+        with open(lammps_inp_out, "w") as lio:
+            lio.write(inptext)
