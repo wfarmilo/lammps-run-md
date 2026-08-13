8dd5139ee9aeda17491c3cf52aa6e00d2d0ba38b
wfarmilo <waf25@cam.ac.uk>
2026-08-13 14:32:17 +0100

    more work on classical production pipeline

---
+++ Code/CL-production/templates/submit.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#SBATCH --job-name=run-lammps-md
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=128
+#SBATCH --cpus-per-task=32
+#SBATCH --time=06:00:00
+#SBATCH --account=e898
+#SBATCH --partition=standard
+#SBATCH --qos=standard
+#SBATCH --output=run-lammps.out
+
+
+export WORKDIR=$(realpath "/work/e898/e898/${USER}")
+
+module load PrgEnv-gnu
+export CRAYPE_LINK_TYPE=dynamic
+
+source "${WORKDIR}/.venv/bin/activate"
+
+# Ensure OMP_NUM_THREADS is consistent with cpus-per-task above
+export OMP_NUM_THREADS=1
+export SRUN_CPUS_PER_TASK=${SLURM_CPUS_PER_TASK}
+
+
+# 
