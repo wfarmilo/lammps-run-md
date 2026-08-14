94107e1ec22ab6b31a30e10167f0ba1937962ae0
wfarmilo <waf25@cam.ac.uk>
2026-08-14 14:53:44 +0100

    add bead convergence runfiles

---
+++ Code/PI-production/bead-convergence/setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Check directory
+cd ../../PI-production/bead-convergence || exit 1
+
+# Switch to PI-production parent dir
+cd ..
+
+current_dir=$(realpath ".")
+templates_dir="${current_dir}/templates"
+
+# Create temporary run parameters
+temps=(200) #(200 250 300)
+beads=(1) #(1 2 4 8 16 32 64)
+pitypes=(PILE) #(PILE ECON)
+
+
+for pi in ${pitypes[*]}
+do
+    for T in ${temps[*]}
+    do
+        for P in ${beads[*]}
+        do
+            outpref="conv-${pi}-${T}-${P}"
+
+            sed -e "s|XXXPIMDTYPEXXX|${pi}|g"       \
+                -e "s|XXXTEMPXXX|${T}|g"            \
+                -e "s|XXXNBEADSXXX|${P}|g"          \
+                -e "s|XXXOUTPREFXXX|${outpref}|g"   \
+                "${templates_dir}/bead-convergence.json" > "${templates_dir}/temp.json"
+
+            python setup.py 0 0 0 -ff "${templates_dir}/temp.json"
+
+            rm "${templates_dir}/temp.json"
+        done
+    done
+done
