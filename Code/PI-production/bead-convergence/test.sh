3d66ddad0eb974ea85ede0ec840052c770a7e749
wfarmilo <waf25@cam.ac.uk>
2026-08-14 15:41:04 +0100

    add test run for bead-convergence pipeline

---
+++ Code/PI-production/bead-convergence/test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Directory check
+cd ../../PI-production/bead-convergence || exit 1
+execdir=$(realpath "../")
+
+
+dftype=$1
+temp=$2
+pitype=$3
+nbeads=$4
+
+runpref="conv-${pitype}-T${temp}-P${nbeads}"
+folder="${dftype}-00/${runpref}"
+echo "Running ${runpref} from ${folder}"
+
+
+# Change to work directory
+workdir=$(realpath "../out/${folder}")
+
+cd $workdir || exit 1
+
+sbatch --time=20 --qos=short --output="${workdir}/slurm.log" "${execdir}/templates/submit.sh" ${runpref}
