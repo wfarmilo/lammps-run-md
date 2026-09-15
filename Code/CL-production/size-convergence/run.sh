7eede566ef6bd36e7842a08bab733dc8f9989f20
wfarmilo <waf25@cam.ac.uk>
2026-09-15 11:14:08 +0100

    remove string wrapper from arrays (will break code)

--- Code/CL-production/size-convergence/run.sh
+++ Code/CL-production/size-convergence/run.sh
@@ -16,13 +16,13 @@ dftypes=$(jq -r '.defect_types[]' "${execdir}/templates/size-convergence.json")
 run_nums=$(jq -r '.run_indices[]' "${execdir}/templates/size-convergence.json")
 sizes=$(jq -r '.pdb_sizes[]' "${execdir}/templates/size-convergence.json")
 
-for dftype in "${dftypes[*]}"
+for dftype in ${dftypes[*]}
 do
-    for run in "${run_nums[*]}"
+    for run in ${run_nums[*]}
     do
-        for T in "${temps[*]}"
+        for T in ${temps[*]}
         do
-            for s in "${sizes[*]}"
+            for s in ${sizes[*]}
             do
                 runpref="conv-T${T}-N${s}"
 
